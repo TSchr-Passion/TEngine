29f90e9270f3e7508959f657320fa019a7653dd7
SchrTh <SchrTh@pgw102.passion.group.local>
2014-07-04 15:01:21 +0200

    Texture of Rocks as placeholder

--- RLike/src/Main.java
+++ RLike/src/Main.java
@@ -1,53 +1,157 @@
 import org.lwjgl.LWJGLException;
-import org.lwjgl.opengl.*;
-
+import org.lwjgl.Sys;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
+import org.newdawn.slick.util.ResourceLoader;
 
 public class Main {
+
+	private Texture texture;
 	
-	public void start()
-	{
+	/** position of quad */
+	float x = 400, y = 300;
+	/** angle of quad rotation */
+	float rotation = 0;
+	
+	/** time at last frame */
+	long lastFrame;
+	
+	/** frames per second */
+	int fps;
+	/** last fps time */
+	long lastFPS;
+
+	public void start() {
 		try {
-			Display.setDisplayMode(new DisplayMode(800,600));
+			Display.setDisplayMode(new DisplayMode(800, 600));
 			Display.create();
-			
-			
+			Display.setVSyncEnabled(true);
 		} catch (LWJGLException e) {
 			e.printStackTrace();
 			System.exit(0);
 		}
+
+		initGL(); // init OpenGL
+		getDelta(); // call once before loop to initialise lastFrame
+		lastFPS = getTime(); // call before loop to initialise fps timer
+
+		while (!Display.isCloseRequested()) {
+			int delta = getDelta();
+			
+			update(delta);
+			renderGL();
+
+			Display.update();
+			Display.sync(60); // cap fps to 60fps
+		}
+
+		Display.destroy();
+	}
+	
+	public void update(int delta) {
+		// rotate quad
+		//rotation += 0.15f * delta;
 		
-		// init OpenGL here
+		if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) x -= 0.35f * delta;
+		if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) x += 0.35f * delta;
+		
+		if (Keyboard.isKeyDown(Keyboard.KEY_UP)) y += 0.35f * delta;
+		if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) y -= 0.35f * delta;
+		
+		// keep quad on the screen
+		if (x < 0) x = 0;
+		if (x > 800) x = 800;
+		if (y < 0) y = 0;
+		if (y > 600) y = 600;
+		
+		updateFPS(); // update FPS Counter
+	}
+	
+	/** 
+	 * Calculate how many milliseconds have passed 
+	 * since last frame.
+	 * 
+	 * @return milliseconds passed since last frame 
+	 */
+	public int getDelta() {
+	    long time = getTime();
+	    int delta = (int) (time - lastFrame);
+	    lastFrame = time;
+	 
+	    return delta;
+	}
+	
+	/**
+	 * Get the accurate system time
+	 * 
+	 * @return The system time in milliseconds
+	 */
+	public long getTime() {
+	    return (Sys.getTime() * 1000) / Sys.getTimerResolution();
+	}
+	
+	/**
+	 * Calculate the FPS and set it in the title bar
+	 */
+	public void updateFPS() {
+		if (getTime() - lastFPS > 1000) {
+			Display.setTitle("FPS: " + fps);
+			fps = 0;
+			lastFPS += 1000;
+		}
+		fps++;
+	}
+	
+	public void initGL() {
 		GL11.glMatrixMode(GL11.GL_PROJECTION);
 		GL11.glLoadIdentity();
 		GL11.glOrtho(0, 800, 0, 600, 1, -1);
 		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 		
-		while (!Display.isCloseRequested())
-		{
-			// Clear the screen and depth buffer
-			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);	
-					
-			// set the color of the quad (R,G,B,A)
-			GL11.glColor3f(0.2f,0.5f,1.0f);
-				
-			// draw quad
-			GL11.glBegin(GL11.GL_QUADS);
-			    GL11.glVertex2f(100,100);
-			    GL11.glVertex2f(100+200,100);
-			    GL11.glVertex2f(100+200,100+200);
-			    GL11.glVertex2f(100,100+200);
-			GL11.glEnd();
-			
-			// render OpenGL here
-			Display.update();
+		try {
+			texture = TextureLoader.getTexture("PJPG", ResourceLoader.getResourceAsStream("res/text_rock.jpg"));
 		}
+		catch(Exception e)
+		{e.printStackTrace();}
+	}
+
+	public void renderGL() {
+		// Clear The Screen And The Depth Buffer
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+
+		// R,G,B,A Set The Color To Blue One Time Only
+		GL11.glColor3f(0.5f, 0.5f, 1.0f);
+		texture.bind();
 		
-		Display.destroy();
+		// draw quad
+		GL11.glPushMatrix();
+			GL11.glTranslatef(x, y, 0);
+			GL11.glRotatef(rotation, 0f, 0f, 1f);
+			GL11.glTranslatef(-x, -y, 0);
+			
+			GL11.glBegin(GL11.GL_QUADS);
+				GL11.glTexCoord2f(0,0);
+				GL11.glVertex2f(x - 50, y - 50);
+				GL11.glTexCoord2f(1,0);
+				GL11.glVertex2f(x + 50, y - 50);
+				GL11.glTexCoord2f(1,1);
+				GL11.glVertex2f(x + 50, y + 50);
+				GL11.glTexCoord2f(0,1);
+				GL11.glVertex2f(x - 50, y + 50);
+			GL11.glEnd();
+		GL11.glPopMatrix();
 	}
 	
-	public static void main(String[] argv)
-	{
-		Main displayExample = new Main();
-		displayExample.start();
+	public static void main(String[] argv) {
+		Main Main = new Main();
+		Main.start();
 	}
 }
