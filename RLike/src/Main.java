a02495a685417333cfc54331e3bdca0e8276c526
SchrTh <SchrTh@pgw102.passion.group.local>
2014-07-11 15:20:43 +0200

    Shaders tutorial stuff

--- RLike/src/Main.java
+++ RLike/src/Main.java
@@ -1,171 +1,84 @@
-import org.lwjgl.LWJGLException;
-import org.lwjgl.Sys;
-import org.lwjgl.input.Keyboard;
-import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.DisplayMode;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL13;
-import org.newdawn.slick.Color;
-import org.newdawn.slick.opengl.Texture;
-import org.newdawn.slick.opengl.TextureLoader;
-import org.newdawn.slick.util.ResourceLoader;
 
-public class Main {
+import java.io.*;
 
-	private Texture texture[];
-	
-	/** position of quad */
-	float x = 400, y = 300;
-	/** angle of quad rotation */
-	float rotation = 0;
-	
-	/** time at last frame */
-	long lastFrame;
-	
-	/** frames per second */
-	int fps;
-	/** last fps time */
-	long lastFPS;
+import org.lwjgl.opengl.*;
 
-	public void start() {
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL11.*;
+
+
+
+public class Main
+{
+	public static void main(String args[])
+	{
 		try {
-			Display.setDisplayMode(new DisplayMode(800, 600));
+			Display.setDisplayMode(new DisplayMode(800,600));
+			Display.setTitle("test");
 			Display.create();
-			Display.setVSyncEnabled(true);
-		} catch (LWJGLException e) {
-			e.printStackTrace();
-			System.exit(0);
 		}
-
-		initGL(); // init OpenGL
-		getDelta(); // call once before loop to initialise lastFrame
-		lastFPS = getTime(); // call before loop to initialise fps timer
-
-		while (!Display.isCloseRequested()) {
-			int delta = getDelta();
+		catch(Exception e){Display.destroy();System.exit(1);}
+		
+		int shaderProgram=glCreateProgram();
+		int vertexShader=glCreateShader(GL_VERTEX_SHADER);
+		int fragmentShader=glCreateShader(GL_FRAGMENT_SHADER);
+		
+		StringBuilder vsSource=new StringBuilder();
+		StringBuilder fsSource=new StringBuilder();
+		
+		try {
+			BufferedReader r=new BufferedReader(new FileReader("src/simple01.vs"));
+			String line;
+			while((line=r.readLine())!=null){vsSource.append(line+"\n");}
+			r.close();
 			
-			update(delta);
-			renderGL();
-
+			r=new BufferedReader(new FileReader("src/simple01.fs"));
+			while((line=r.readLine())!=null){fsSource.append(line+"\n");}
+			r.close();
+		}
+		catch(Exception e){
+			Display.destroy();System.exit(1);
+		}
+		glShaderSource(vertexShader,vsSource);
+		glCompileShader(vertexShader);
+		if (glGetShaderi(vertexShader,GL_COMPILE_STATUS) == GL_FALSE)  {System.out.println("error");}
+		
+		glShaderSource(fragmentShader,fsSource);
+		glCompileShader(fragmentShader);
+		if (glGetShaderi(fragmentShader,GL_COMPILE_STATUS) == GL_FALSE)  {System.out.println("error");}
+		
+		glAttachShader(shaderProgram,vertexShader);
+		glAttachShader(shaderProgram,fragmentShader);
+		
+		glLinkProgram(shaderProgram);
+		glValidateProgram(shaderProgram);
+		
+		while(!Display.isCloseRequested())
+		{
+			glUseProgram(shaderProgram);
+			
+			glBegin(GL_TRIANGLES);
+			glColor3f(1,0,0);
+			glVertex2f(-0.5f,-0.5f);
+			glColor3f(0,1,0);
+			glVertex2f(0.5f,-0.5f);
+			glColor3f(0,0,1);
+			glVertex2f(0.0f,0.5f);
+			glEnd();
+			
+			glUseProgram(0);
+			
 			Display.update();
-			Display.sync(60); // cap fps to 60fps
-		}
-
-		Display.destroy();
-	}
-	
-	public void update(int delta) {
-		// rotate quad
-		rotation += 0.10f * delta;
-		
-		if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) x -= 0.35f * delta;
-		if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) x += 0.35f * delta;
-		
-		if (Keyboard.isKeyDown(Keyboard.KEY_UP)) y += 0.35f * delta;
-		if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) y -= 0.35f * delta;
-		
-		// keep quad on the screen
-		if (x < 0) x = 0;
-		if (x > 800) x = 800;
-		if (y < 0) y = 0;
-		if (y > 600) y = 600;
-		
-		updateFPS(); // update FPS Counter
-	}
-	
-	/** 
-	 * Calculate how many milliseconds have passed 
-	 * since last frame.
-	 * 
-	 * @return milliseconds passed since last frame 
-	 */
-	public int getDelta() {
-	    long time = getTime();
-	    int delta = (int) (time - lastFrame);
-	    lastFrame = time;
-	 
-	    return delta;
-	}
-	
-	/**
-	 * Get the accurate system time
-	 * 
-	 * @return The system time in milliseconds
-	 */
-	public long getTime() {
-	    return (Sys.getTime() * 1000) / Sys.getTimerResolution();
-	}
-	
-	/**
-	 * Calculate the FPS and set it in the title bar
-	 */
-	public void updateFPS() {
-		if (getTime() - lastFPS > 1000) {
-			Display.setTitle("FPS: " + fps);
-			fps = 0;
-			lastFPS += 1000;
-		}
-		fps++;
-	}
-	
-	public void initGL() {
-		GL11.glMatrixMode(GL11.GL_PROJECTION);
-		GL11.glLoadIdentity();
-		GL11.glOrtho(0, 800, 0, 600, 1, -1);
-		GL11.glMatrixMode(GL11.GL_MODELVIEW);
-		GL11.glEnable(GL11.GL_TEXTURE_2D);
-		GL11.glEnable(GL11.GL_BLEND);
-		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-		
-		texture=new Texture[2];
-		try {
-			texture[0] = TextureLoader.getTexture("JPG", ResourceLoader.getResourceAsStream("res/tex_rock.jpg"));
-		}
-		catch(Exception e){e.printStackTrace();}
-		GL13.glActiveTexture(GL13.GL_TEXTURE0+0);texture[0].bind();
-		
-		try {
-			texture[1] = TextureLoader.getTexture("JPG", ResourceLoader.getResourceAsStream("res/tex_grass.jpg"));
-		}
-		catch(Exception e){e.printStackTrace();}
-		GL13.glActiveTexture(GL13.GL_TEXTURE0+1);texture[1].bind();
-		//GL13.glActiveTexture(0);
-	}
-
-	public void renderGL() {
-		// Clear The Screen And The Depth Buffer
-		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-
-		// R,G,B,A Set The Color To Blue One Time Only
-		//GL11.glColor3f(0.5f, 0.5f, 1.0f);
-		
-		//texture[0].bind();
-		//GL13.glActiveTexture(GL13.GL_TEXTURE0);
-		// draw quad
-		GL11.glPushMatrix();
-			GL11.glTranslatef(x, y, 0);
-			GL11.glRotatef(rotation, 0f, 0f, 1f);
-			GL11.glTranslatef(-x, -y, 0);
+			Display.sync(60);
 			
-			GL11.glBegin(GL11.GL_QUADS);
-				//GL13.glMultiTexCoord2f(GL13.GL_TEXTURE0+1, 0, 0);
-				GL11.glTexCoord2f(0,0);
-				GL11.glVertex2f(x - 50, y - 50);
-				//GL13.glMultiTexCoord2f(GL13.GL_TEXTURE0+1, 1, 0);
-				GL11.glTexCoord2f(1,0);
-				GL11.glVertex2f(x + 50, y - 50);
-				//GL13.glMultiTexCoord2f(GL13.GL_TEXTURE0+1, 1, 1);
-				GL11.glTexCoord2f(1,1);
-				GL11.glVertex2f(x + 50, y + 50);
-				//GL13.glMultiTexCoord2f(GL13.GL_TEXTURE0+1, 0, 1);
-				GL11.glTexCoord2f(0,1);
-				GL11.glVertex2f(x - 50, y + 50);
-			GL11.glEnd();
-		GL11.glPopMatrix();
-	}
-	
-	public static void main(String[] argv) {
-		Main Main = new Main();
-		Main.start();
+			
+		}
+		
+		glDeleteProgram(shaderProgram);
+		glDeleteShader(fragmentShader);
+		glDeleteShader(vertexShader);
+		
+		Display.destroy();
+		System.exit(0);
 	}
 }
